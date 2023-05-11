09923fcb6386a43a12e81538d0aafb49ce4c4eaa
Poomon001 <57118681+Poomon001@users.noreply.github.com>
2023-05-11 09:21:16 -0700

    update output format

--- A00/Q2/word_count.c
+++ A00/Q2/word_count.c
@@ -4,14 +4,14 @@
 int countWordFromFile(char* argv[]);
 
 int main(int argc, char* argv[]) {
-    printf("%d %s", countWordFromFile(argv), argv[1]);
+    printf("%d %s\n", countWordFromFile(argv), argv[1]);
     return 0;
 }
 
 int countWordFromFile(char* argv[]) {
     FILE* fpt = fopen(argv[1], "r");
-    char c;
-    char prev = ' ';
+    int c;
+    int prev = ' ';
     int wordCount = 0;
 
     while((c = fgetc(fpt)) != EOF) {
